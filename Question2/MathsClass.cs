4f5e7b40b7c7d5902cf5190d914debb3bb021a1f
reginald160 <ozougwuifeanyi160@gmail.com>
2020-05-07 13:19:15 +0100

    .

---
+++ Question2/MathsClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MulltiCastDelegateApp
+{
+	public class MathsClass
+	{
+		
+
+		static public void  Multiplication (int num1, int num2)
+		{
+			Console.WriteLine (" The result from multiplication method is " + num1 * num2);
+
+		}
+
+
+
+
+		static public void Addition (int num1, int num2)
+		{
+
+			Console.WriteLine (" The result from Addition method is " + num1 + num2);
+
+		}
+
+
+
+	}
+}
+
---
+++ Question2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MulltiCastDelegateApp
+{
+	public delegate void ArithemticDelegate (int a, int b);
+	
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			ArithemticDelegate myArithmetic = MathsClass.Multiplication;
+			myArithmetic += MathsClass.Addition;
+
+			myArithmetic.Invoke (12, 34);
+		}
+	}
+}
